eb7b6493413ea656a39839ee27f5fd04298eaa05
unknown <alexmusicalsa@gmail.com>
2022-11-06 23:28:59 -0300

    Continuação das validações

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -3,8 +3,15 @@ function enviarDados() {
   var codigo = document.getElementById("codigo").value;
   var quantidade = document.getElementById("quantidade").value;
   var valor = document.getElementById("valor").value;
-  var imagem = document.getElementById("imagem").value;
 
   if (produto.length < 3 || produto.length > 30)
     alert("Tamanho do nome do produto invalido");
 }
+
+function validar(){
+var cod = document.getElementById("input-cod").value;
+if (cod.value == ""){
+  alert("Erro!");
+}
+}
+
