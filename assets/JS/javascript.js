d50b021b2a4c3b9818c03a7a1f16dd6480f4e0bd
unknown <alexmusicalsa@gmail.com>
2022-11-07 20:31:33 -0300

    Continuação...

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,17 +1,10 @@
-function enviarDados() {
-  var produto = document.getElementById("produto").value;
-  var codigo = document.getElementById("codigo").value;
-  var quantidade = document.getElementById("quantidade").value;
-  var valor = document.getElementById("valor").value;
+function validar() {
+  var produto = document.getElementById("input-prod").value;
+  var codigo = document.getElementById("input-cod").value;
+  var quantidade = document.getElementById("input-qtd").value;
+  var valor = document.getElementById("input-val").value;
 
-  if (produto.length < 3 || produto.length > 30)
-    alert("Tamanho do nome do produto invalido");
-}
 
-function validar(){
-var cod = document.getElementById("input-cod").value;
-if (cod.value == ""){
-  alert("Erro!");
-}
-}
+  
+
 
