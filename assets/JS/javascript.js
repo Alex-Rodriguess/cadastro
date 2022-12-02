ad8cf658336ad1bb91eba22f5a630173d2beb855
Alex Rodrigues <alexmusicalsa@gmail.com>
2022-12-01 22:47:30 -0300

    Add

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,13 +1,15 @@
-function validar() {
+function validar(frm) {
   var produto = document.getElementById("input-prod").value;
   var codigo = document.getElementById("input-cod").value;
   var quantidade = document.getElementById("input-qtd").value;
   var valor = document.getElementById("input-val").value;
 
-  if (produto = ""){
+  if (produto.value = "") {
     document.querySelector("input-prod").focus;
   }
 }
-  
 
+if (frm.codigo.value == "") || codigo.value == null{
+  alert("Insira um código válido!");
+}
 
